2f43cb89a1c0fb71c900f5269615841051d423af
Sujay Khandekar <sskhandek@gmail.com>
2015-05-24 23:39:26 -0700

    Redesign started

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -9,9 +9,33 @@
  */
 angular.module('sujayKhandekarApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.contactLinks = [
+      {
+        "name": "Facebook",
+        "icon":"fa fa-fw fa-facebook fa-2x",
+        "link":"https://www.facebook.com/sujay.khandekar"
+      },
+      {
+        "name": "Google+",
+        "icon":"fa fa-fw fa-google-plus fa-2x",
+        "link":"https://plus.google.com/105856798281199967470/posts"
+      },
+      {
+        "name": "Twitter",
+        "icon":"fa fa-fw fa-twitter fa-2x",
+        "link":"https://twitter.com/sujaykhandekar"
+      },
+      {
+        "name": "LinkedIn",
+        "icon":"fa fa-fw fa-linkedin fa-2x",
+        "link":"https://www.linkedin.com/in/sujaykhandekar"
+      },
+      {
+        "name": "Resume",
+        "icon":"fa fa-fw fa-file-text-o fa-2x",
+        "link":"images/resume.pdf"
+      }
     ];
+
+
   });
