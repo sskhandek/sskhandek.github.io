c31237504bc54120713499f582602186fc479b90
Sujay Khandekar <sskhandek@gmail.com>
2015-05-25 01:30:58 -0700

    Redesign in progress

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -12,29 +12,40 @@ angular.module('sujayKhandekarApp')
     $scope.contactLinks = [
       {
         "name": "Facebook",
-        "icon":"fa fa-fw fa-facebook fa-2x",
+        "icon":"facebook",
         "link":"https://www.facebook.com/sujay.khandekar"
       },
       {
         "name": "Google+",
-        "icon":"fa fa-fw fa-google-plus fa-2x",
+        "icon":"fa fa-google-plus fa-2x",
         "link":"https://plus.google.com/105856798281199967470/posts"
       },
       {
         "name": "Twitter",
-        "icon":"fa fa-fw fa-twitter fa-2x",
+        "icon":"fa fa-twitter fa-2x",
         "link":"https://twitter.com/sujaykhandekar"
       },
       {
         "name": "LinkedIn",
-        "icon":"fa fa-fw fa-linkedin fa-2x",
+        "icon":"fa fa-linkedin fa-2x",
         "link":"https://www.linkedin.com/in/sujaykhandekar"
       },
       {
+        "name": "Quora",
+        "link": "http://www.quora.com/Sujay-Khandekar-1"
+      },
+      {
         "name": "Resume",
-        "icon":"fa fa-fw fa-file-text-o fa-2x",
+        "icon":"fa fa-file-text-o fa-2x",
         "link":"images/resume.pdf"
       }
+
+    ];
+
+    $scope.companies = [
+        "https://pbs.twimg.com/profile_images/568847126793175040/05a1ib0K.png",
+        "http://a1031.phobos.apple.com/us/r1000/018/Purple4/v4/a1/2c/40/a12c4071-c770-f925-8fd4-1be7584f9586/V4HttpAssetRepositoryClient-mzl.gflvstsz.png-6095098972935482443.png",
+        "http://brandevolutionist.com/wp-content/uploads/2015/04/square-facebook-512.png"
     ];
 
 
